0cd6fea322c564852c0a886c7b394ac480009c63
Eli Ilushin <elii@codevalue.net>
2017-09-17 21:02:07 +0300

    task dtos

---
+++ Source/CodeValue.ScrumBoard.Service/DTOs/TaskRequestResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task = CodeValue.ScrumBoard.Service.Entities.Task;
+
+namespace CodeValue.ScrumBoard.Service.DTOs
+{
+    public class TaskRequestResult
+    {
+        public IEnumerable<Task> Tasks { get; set; }
+    }
+}
