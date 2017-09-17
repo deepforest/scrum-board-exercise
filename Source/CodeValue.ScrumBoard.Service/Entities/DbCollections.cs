be9b335c6eb76a9ea346bb2816390018b5287681
tomers <tomer.shamam@gmail.com>
2017-09-17 20:10:06 +0300

    Added missing fields to data entities. Added DbCollections with all mongodb collections.

---
+++ Source/CodeValue.ScrumBoard.Service/Entities/DbCollections.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeValue.ScrumBoard.Service.Entities
+{
+    public class DbCollections
+    {
+        public const string Users = "Users";
+        public const string Boards = "Boards";
+        public const string Tasks = "Tasks";
+    }
+}
