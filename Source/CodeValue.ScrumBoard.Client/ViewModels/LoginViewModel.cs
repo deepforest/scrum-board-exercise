4752fba521d753579447ff9b9752b4fee6451f59
DESKTOP-L2IS484\fadi_ <fadi.salfiti.63@gmail.com>
2017-09-17 22:11:18 +0300

    Login

--- Source/CodeValue.ScrumBoard.Client/ViewModels/LoginViewModel.cs
+++ Source/CodeValue.ScrumBoard.Client/ViewModels/LoginViewModel.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Caliburn.Micro;
 namespace CodeValue.ScrumBoard.Client.ViewModels
 {
     public class LoginViewModel
     {
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (_password.Equals(value))
+                    return;
+                _password = value;
+
+            }
+        }
+
+        public string UserName { get; set; }
+
     }
 }
