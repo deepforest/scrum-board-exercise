a9ac30389b1b8a67984c844c0e083a1cf38e9b3e
ahmad abo alhega <ahmad.abo.alhega@hotmail.com>
2017-09-18 17:24:04 +0300

    task create and update

--- Source/CodeValue.ScrumBoard.Service/Controllers/TaskController.cs
+++ Source/CodeValue.ScrumBoard.Service/Controllers/TaskController.cs
@@ -1,3 +1,6 @@
+using CodeValue.ScrumBoard.Service.DTOs;
+using CodeValue.ScrumBoard.Service.Entities;
+using CodeValue.ScrumBoard.Service.Managers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeValue.ScrumBoard.Service.Controllers
@@ -5,5 +8,19 @@ namespace CodeValue.ScrumBoard.Service.Controllers
     [Route("api/[controller]")]
     public class TaskController : Controller
     {
+        [HttpPost]
+        public async System.Threading.Tasks.Task<NewTaskResponse> CreateNewTaskAsync([FromBody] NewTaskDetailsRequest newTaskDetailsRequest)
+        {
+            var taskManager = new TaskManager();
+            var taskResponse = await taskManager.CreateTaskAsync(newTaskDetailsRequest);
+            return taskResponse;// TODO:: return CreatRoute
+        }
+
+        public async System.Threading.Tasks.Task<bool> UpdateExistTaskAsync(string taskId, Task fildesToUpdate)
+        {
+            var taskManager = new TaskManager();
+            var updateTaskResul = await taskManager.UpdateTaskAsync(taskId, fildesToUpdate);
+            return updateTaskResul;
+        }
     }
 }
---
+++ Source/CodeValue.ScrumBoard.Service/DTOs/NewTaskResponse.cs
@@ -0,0 +1,13 @@
+﻿using CodeValue.ScrumBoard.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeValue.ScrumBoard.Service.DTOs
+{
+    public class NewTaskResponse
+    {
+        public string Name { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+}
