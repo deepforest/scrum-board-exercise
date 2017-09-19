6d1968fc3551d27b939055f3d6da88a5a99829b3
Muhannad Orabi <mohanad_o3@hotmail.com>
2017-09-19 18:46:27 +0300

    Adding IBoardApi

--- Source/CodeValue.ScrumBoard.Client/ViewModels/BoardViewModel.cs
+++ Source/CodeValue.ScrumBoard.Client/ViewModels/BoardViewModel.cs
@@ -8,6 +8,33 @@ namespace CodeValue.ScrumBoard.Client.ViewModels
 {
     class BoardViewModel
     {
+        public BoardViewModel()
+        {
+            TodoTasks = new ObservableCollection<TaskModel>();
+            DoingTasks = new ObservableCollection<TaskModel>();
+            DoneTasks = new ObservableCollection<TaskModel>();
+        }
 
+        public ObservableCollection<TaskModel> TodoTasks { get; private set; }
+        public ObservableCollection<TaskModel> DoingTasks { get; private set; }
+        public ObservableCollection<TaskModel> DoneTasks { get; private set; }
+
+        //public async Task GetAllStudentsAsync()
+        //{
+        //    try
+        //    {
+        //        var api = RestService.For<IBoardApi>("http://localhost:61151/api");
+        //        var tasks = await api.GetBoardTasksAsync();
+        //        if (tasks == null)
+        //            return;
+
+        //        //foreach (var student in students)
+        //        //    Students.Add(student);
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //}
     }
 }
--- Source/CodeValue.ScrumBoard.Client/Views/BoardView.xaml.cs
+++ Source/CodeValue.ScrumBoard.Client/Views/BoardView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CodeValue.ScrumBoard.Client.Views
@@ -17,7 +18,7 @@ namespace CodeValue.ScrumBoard.Client.Views
     /// <summary>
     /// Interaction logic for BoardView.xaml
     /// </summary>
-    public partial class BoardView : Window
+    public partial class BoardView : UserControl
     {
         public BoardView()
         {
