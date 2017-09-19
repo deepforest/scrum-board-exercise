a007c0e41eb10f728253acb85be6f03b50d34c3c
Adam Sharon <adamsharon3@gmail.com>
2017-09-19 11:46:45 +0300

    Fixed MVVM implementation

--- Source/CodeValue.ScrumBoard.Client/ViewModels/MainViewModel.cs
+++ Source/CodeValue.ScrumBoard.Client/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
-using CodeValue.ScrumBoard.Client.Common.Types;
+
 using CodeValue.ScrumBoard.Client.Events;
+using CodeValue.ScrumBoard.Client.Navigation;
 using CodeValue.ScrumBoard.Client.Views;
 using System;
 using System.Collections.Generic;
@@ -9,69 +10,136 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace CodeValue.ScrumBoard.Client.ViewModels
 {
-    public sealed class MainViewModel : Conductor<Screen>.Collection.AllActive,
-                                        IHandle<WindowStateChangedEvent>
+    public sealed class MainViewModel : Conductor<Screen>.Collection.OneActive                                      
     {
 
-        public Screen TitleBarScreen => Items[0];  
-        // We would like to have an instance of the main window so we can control its state (minimized,maximized...).
-        private Window _mainWindow;
+        private WindowState _currentWindowState;
 
-        
-        public MainViewModel(IEventAggregator eventAggregator,                               
-                            TitleBarViewModel titleBarViewModel,
-                            LoginViewModel loginViewModel)
+        private Visibility _progressBarVisibility;
+
+        private Func<ILoginViewModel> _loginViewModelCreator;
+
+        private string _currentUserName;
+        private ImageSource _userImage;
+
+     
+                     
+        public MainViewModel(IEventAggregator eventAggregator,                                                           
+                            Func<ILoginViewModel> loginViewModelCreator                                                       )
         {
+            _progressBarVisibility = Visibility.Collapsed;
             eventAggregator.Subscribe(this);
-            DisplayName = "Virtual Scrumn Board";            
-            Items.AddRange(new Screen[] 
-            {
-               titleBarViewModel,
-               loginViewModel
+            _loginViewModelCreator = loginViewModelCreator;
+
+            Items.AddRange(new Screen[]
+            {               
+               loginViewModelCreator() as LoginViewModel
             });
-          
+
+            CurrentWindowState = WindowState.Normal;
+            UserImage = new BitmapImage(new Uri(@"/Images/unknown_user.png", UriKind.Relative));
+            CurrentUserName = "guest";
+
         }
 
-        protected override void OnViewAttached(object view, object context)
+        public WindowState CurrentWindowState
         {
-            // TODO: handle null case.
-            _mainWindow = (Window)GetView();
-            base.OnViewAttached(view, context);
-        }
-
-        /// <summary>
-        /// Handle the changes of the windows state that comes from the TitleBarViewModel clicks.
-        /// </summary>
-        /// <param name="message"></param>
-        public void Handle(WindowStateChangedEvent message)
-        {
-            // TODO: make sure that _mainWindow is not null.
-            switch (message.WindowStateType)
+            get
             {
-                case WindowStateType.Closed:                   
-                    Console.WriteLine("Closed");
-                    _mainWindow.Close();
-                    break;
-                case WindowStateType.Maximized:                   
-                    _mainWindow.WindowState = WindowState.Maximized;
-                    Console.WriteLine("Maximized");
-                    break;
-                case WindowStateType.Minimized:
-                    _mainWindow.WindowState = WindowState.Minimized;
-                    //  Console.WriteLine("Minimized");
-                    break;
-                case WindowStateType.Normal:                    
-                    _mainWindow.WindowState = WindowState.Normal;
-                    break;
-            }            
+                return _currentWindowState;
+            }
+            set
+            {
+                _currentWindowState = value;
+                NotifyOfPropertyChange(() => CurrentWindowState);
+            }
         }
 
-        private void Navigate()
+        public Visibility ProgressBarVisibility
         {
-            Items[0] = Items[1] ;            
+            get { return _progressBarVisibility; }
+            set
+            {
+                if (Equals(_progressBarVisibility, value))
+                    return;
+                _progressBarVisibility = value;
+                NotifyOfPropertyChange(() => ProgressBarVisibility);
+            }
         }
+
+        public string CurrentUserName 
+         { 
+             get => _currentUserName; 
+             set 
+             { 
+                 if (Equals(_currentUserName, value)) 
+                     return;   
+                 _currentUserName = value; 
+                 NotifyOfPropertyChange(() => CurrentUserName); 
+             } 
+         } 
+  
+         public ImageSource UserImage
+         { 
+             get => _userImage; 
+             set 
+             { 
+                 if (Equals(_userImage, value)) 
+                     return;   
+                 _userImage = value; 
+                 NotifyOfPropertyChange(() => UserImage); 
+             } 
+         } 
+ 
+ 
+         protected override void OnInitialize()
+         { 
+             ActiveItem = Items.First(); 
+         } 
+
+
+      
+        public void CloseWindow()
+        {
+            TryClose();
+        }
+
+        public void MaximizeWindow()
+        {
+            CurrentWindowState = WindowState.Maximized;
+        }
+
+        public void MinimizeWindow()
+        {
+            CurrentWindowState = WindowState.Minimized;
+        }
+
+        public void NormalizeWindow()
+        {
+            CurrentWindowState = WindowState.Normal;
+        }
+
+        private async void Navigate(INavigation navigation)
+        {
+            try
+            {
+                ProgressBarVisibility = Visibility.Visible;
+                ActiveItem = await navigation.NavigateToAsync();            
+            }
+            finally
+            {
+                ProgressBarVisibility = Visibility.Collapsed;
+            }
+        }
+
+      
+
+    
     }
 }
