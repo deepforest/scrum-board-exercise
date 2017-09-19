eff49dca4ed13cc272e98a1ae5b1f054f54020b9
DESKTOP-L2IS484\fadi_ <fadi.salfiti.63@gmail.com>
2017-09-19 20:33:07 +0300

    Task dto's

---
+++ Source/CodeValue.ScrumBoard.Service/DTOs/NewTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeValue.ScrumBoard.Service.DTOs
+{
+    public class NewTask
+    {
+        public string CreatedBy { get; set; }
+        public string Description { get; set; }
+        public string AssignedTo { get; set; }
+        public int Priority { get; set; }
+        public int RemainingWork { get; set; }
+    }
+}
---
+++ Source/CodeValue.ScrumBoard.Service/DTOs/UpdateTask.cs
@@ -0,0 +1,18 @@
+﻿using CodeValue.ScrumBoard.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeValue.ScrumBoard.Service.DTOs
+{
+    public class UpdateTask
+    {
+        public string AssignedTo { get; set; }
+        public string Description { get; set; }
+        public Entities.TaskStatus Status { get; set; }
+        public int Priority { get; set; }
+        public int RemainingWork { get; set; }
+        public Comment[] Comments { get; set; }
+    }
+}
