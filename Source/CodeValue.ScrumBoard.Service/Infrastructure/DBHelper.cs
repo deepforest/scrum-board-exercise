6d08b75f1a30ca3462b3c43666844749c40592d9
DESKTOP-L2IS484\fadi_ <fadi.salfiti.63@gmail.com>
2017-09-19 17:41:52 +0300

    Add infrastructure

--- Source/CodeValue.ScrumBoard.Service/Infrastructure/DBHelper.cs
+++ Source/CodeValue.ScrumBoard.Service/Infrastructure/DBHelper.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CodeValue.ScrumBoard.Service.Entities
+namespace CodeValue.ScrumBoard.Service.Infrastructure
 {
     public static class DBHelper
     {
