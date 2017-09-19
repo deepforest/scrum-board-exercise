d17a315d1c378e8ef119453e20639971216bfef8
hamam massarweh <hamam.massarweh@hotmail.com>
2017-09-19 10:05:26 +0300

    Delete Task service added

--- Source/CodeValue.ScrumBoard.Service/Controllers/TaskController.cs
+++ Source/CodeValue.ScrumBoard.Service/Controllers/TaskController.cs
@@ -1,9 +1,18 @@
+using CodeValue.ScrumBoard.Service.Managers;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace CodeValue.ScrumBoard.Service.Controllers
 {
     [Route("api/[controller]")]
     public class TaskController : Controller
     {
+        [HttpDelete("{id}")]
+        public async System.Threading.Tasks.Task<IActionResult> DeleteTaskAsync(ObjectId id)
+        {
+            var tasksManager = new TaskManager();
+            await tasksManager.DeleteTask(id);
+            return Ok();
+        }
     }
 }
--- Source/CodeValue.ScrumBoard.Service/Managers/TaskManager.cs
+++ Source/CodeValue.ScrumBoard.Service/Managers/TaskManager.cs
@@ -1,6 +1,24 @@
+using CodeValue.ScrumBoard.Service.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
 namespace CodeValue.ScrumBoard.Service.Managers
 {
     public class TaskManager : ITaskManager
     {
+        public async System.Threading.Tasks.Task DeleteTask(ObjectId id)
+        {
+            var tasksCollection = GetCollection<Task>(DbCollections.Tasks);
+            var Deletetask = await tasksCollection.DeleteOneAsync(Builders<Task>.Filter.Eq("Id", id));
+        }
+
+        private static IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            var db = new MongoClient("mongodb://localhost");
+            var server = db.GetDatabase("scrumboard");
+            var collection = server.GetCollection<T>(collectionName);
+            return collection;
+        }
     }
 }
