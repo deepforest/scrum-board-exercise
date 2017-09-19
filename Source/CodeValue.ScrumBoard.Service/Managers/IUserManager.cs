79c369513c98196d7ee4f81721e4c94eff534385
DESKTOP-L2IS484\fadi_ <fadi.salfiti.63@gmail.com>
2017-09-19 15:39:54 +0300

    Updated User manager

--- Source/CodeValue.ScrumBoard.Service/Managers/IUserManager.cs
+++ Source/CodeValue.ScrumBoard.Service/Managers/IUserManager.cs
@@ -1,7 +1,10 @@
-﻿namespace CodeValue.ScrumBoard.Service.Managers
+﻿using CodeValue.ScrumBoard.Service.Entities;
+using System.Collections.Generic;
+
+namespace CodeValue.ScrumBoard.Service.Managers
 {
     public interface IUserManager
     {
-    
+        IEnumerable<User> GetUsers();
     }
 }
